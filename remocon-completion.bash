4a7fb510c1df2696c7784b267448879fe112d4b2
Jaeho Shin <netj@sparcs.org>
2019-05-11 15:06:46 -0700

    Apache License 2.0

--- remocon-completion.bash
+++ remocon-completion.bash
@@ -2,6 +2,21 @@
 # bash completion for remocon
 # Usage: source remocon-completion.bash
 # Requires: bash-completion 2+
+##
+#   Copyright 2018 Jaeho Shin
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+##
 
 # If there is no ssh completion, but we have the _completion loader try to load it
 if ! declare -F _scp_remote_files > /dev/null && declare -F _completion_loader > /dev/null; then
